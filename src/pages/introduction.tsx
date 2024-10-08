767a7c36e50ee0fceb9429b7c3f48eba35a1126c
AsMuin <asmuins@foxmail.com>
2024-10-09 01:09:25 +0800

    update: 样式模块调整

--- src/pages/introduction.tsx
+++ src/pages/introduction.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import Layout from '@theme/Layout';
-import '../css/pages/introduction.scss';
+import style from '../css/pages/introduction.module.scss';
 export default function Introduction() {
     const textContent =
         '欢迎来到我的个人网站，作为一个刚刚接触Web前端开发的新人，本站作为我平时涉及到的知识记录和总结，主要涉猎于Web前端技术领域。作者搭建的初衷也是尝试个人部署一个属于自己的小避风港。在参与实习之余，拓展自己的开发经历，希望能帮助到更多跟我同样处境的新人。---当然，作者本身技术力有限，部分文案可能存在疏漏或者错误，如果内容出现问题，我欢迎各位通过邮箱的方式跟我进行沟通。---最后，祝你在这里的旅程愉快！';
@@ -11,7 +11,7 @@ export default function Introduction() {
                 animate={{ opacity: 1, x: 0, y: 0 }}
                 transition={{ duration: 2 + index * 0.5 }}
                 key={index}
-                className="text">
+                className={style.text}>
                 {item}
             </motion.p>
         );
@@ -24,7 +24,7 @@ export default function Introduction() {
                 transition={{ duration: 2 }}
                 className="page-introduction"> */}
             <PageTitle />
-            <motion.section className="content">
+            <motion.section className={style.content}>
                 {content}
                 {/* <p className="text">
                         Hello,I&apos;m AsMuin, a web developer and designer. I
@@ -46,7 +46,7 @@ function PageTitle() {
         scale: 1
     };
     return (
-        <motion.h1 className="page-title">
+        <motion.h1 className={style['page-title']}>
             {/* <motion.span
                 initial={initMotion}
                 animate={animateMotion}
@@ -68,10 +68,10 @@ function PageEnd() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 3 }}
-            className="page-end">
-            <h3 className="author">作者：AsMuin</h3>
+            className={style['page-end']}>
+            <h3 className={style.author}>作者：AsMuin</h3>
             <img
-                className="page-end-img"
+                className={style['page-end-img']}
                 src={require('@site/static/img/happy-star.png').default}
                 alt="happy meet you"
             />
